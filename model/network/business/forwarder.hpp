e500f907a1837ae151216a095bfe94273d63d73a
Hatedatastructures <c13738349304@outlook.com>
2025-10-22 16:59:23 +0800

    feat(forwarder): 添加转发器配置结构体并优化构造函数格式
    refactor(coroutine): 实现协程基础功能并添加示例代码
    
    在forwarder.hpp中添加transponder_config结构体用于配置转发目标
    在coroutine.cpp中实现协程基础功能并添加演示示例

--- model/network/business/forwarder.hpp
+++ model/network/business/forwarder.hpp
@@ -11,6 +11,11 @@ namespace represents
    * @brief 基于 `http` 协议的服务端http请求转发器(代理)
    * @details 用于将客户端的http请求转发到指定的http服务器，并将服务器的响应返回给客户端
    */
+  struct transponder_config
+  {
+    std::string _target_host; // 目标主机
+    std::uint16_t _target_port; // 目标端口
+  };
   template<class request_t, class response_t>
   class transponder
   {
@@ -18,10 +23,7 @@ namespace represents
     conversation::session_management<request_t,response_t> _management; // 会话管理
   public:
     transponder(boost::asio::io_context& io_context)
-    : _io_context(io_context), _management(io_context)
-    {
-    }
-
+    : _io_context(io_context), _management(io_context) {}
     /**
      * @brief 启动转发器
      * @return true 启动成功
@@ -31,7 +33,6 @@ namespace represents
     {
       return _management.start();
     }
-
     /**
      * @brief 停止转发器
      * @return true 停止成功
